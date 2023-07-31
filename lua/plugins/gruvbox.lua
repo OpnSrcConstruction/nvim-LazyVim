03f811791511bb784b92c7c69e4ba64572acc0bb
OpnSrcConstruction <Opensourceconstruction.com>
2023-07-31 19:13:30 -0400

    Comment cleanup.

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,7 +1,7 @@
 return {
   -- Add gruvbox theme plugin.
   { "ellisonleao/gruvbox.nvim" },
-  -- Configure LV to use gruvbox.
+  -- Configure LV to use the gruvbox theme.
   {
     "LazyVim/LazyVim",
     opts = {
