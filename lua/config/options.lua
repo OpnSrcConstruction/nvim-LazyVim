af6d791898cf57325c03003c8bb3360a57158dce
zim <zim@grey-top>
2023-08-02 23:15:49 -0400

    Added cursorcolumn for a crosshair retical effect.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- Add back my cursor column.
+-- shortens vim.opt(value) to just opt(key) via local variable assignment(=).
+local opt = vim.opt
+-- Adding back my Cursor column.
+opt.cursorcolumn = true
